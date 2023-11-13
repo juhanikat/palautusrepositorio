67cdb55eea38619000283e1001d9f3d92286d7a1
juhanikat <juhani.kataja@gmail.com>
2023-11-13 16:15:22 +0200

    things

---
+++ viikko2/project-reader/src/project_reader.py
@@ -0,0 +1,20 @@
+from urllib import request
+from project import Project
+import toml
+
+
+class ProjectReader:
+    def __init__(self, url):
+        self._url = url
+
+    def get_project(self):
+        # tiedoston merkkijonomuotoinen sisältö
+        content = request.urlopen(self._url).read().decode("utf-8")
+
+        parsed = toml.loads(content)
+        return Project(parsed['tool']['poetry']['name'], 
+                       parsed['tool']['poetry']['description'], 
+                       parsed['tool']['poetry']['license'], 
+                       parsed['tool']['poetry']['authors'], 
+                       parsed['tool']['poetry']['dependencies'], 
+                       parsed['tool']['poetry']['group']['dev']['dependencies'])
